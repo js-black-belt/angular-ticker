95d1a650420f1bce528906a7a3d41a6ca2d738d5
jsBlackBelt <sefi.ninio@gmail.com>
2015-06-15 22:46:39 +0300

    update release files

--- release/angular-ticker.js
+++ release/angular-ticker.js
@@ -26,38 +26,118 @@ angular.module('jsbb.angularTicker')
         var registrants = {},
             internalInterval = 1000;
 
+        var resetRegistrantState = function (registrant) {
+            registrant.delay = registrant.interval;
+            registrant.isPending = false;
+        };
+
+        var handleBlockingTask = function (registrant) {
+            if (!registrant.isPending) {
+                registrant.isPending = true;
+                registrant.tick().then(function () {
+                    resetRegistrantState(registrant);
+                }, function () {
+                    resetRegistrantState(registrant);
+                });
+            }
+        };
+
+        var handleNonBlockingTask = function (registrant) {
+            registrant.tick();
+            resetRegistrantState(registrant);
+        };
+
+        var tick = function () {
+            angular.forEach(registrants, function (registrant) {
+                // update the delay.
+                registrant.delay -= internalInterval;
+
+                if (registrant.delay <= 0) {
+                    // time to tick!
+                    try {
+                        if (registrant.isBlocking) {
+                            handleBlockingTask(registrant);
+                        } else {
+                            handleNonBlockingTask(registrant);
+                        }
+                    } catch (e) {
+                        console.log(e);
+                        resetRegistrantState(registrant);
+                    }
+                }
+            });
+        };
+
         var start = function () {
-            $interval(service.tick, internalInterval);
-            service.tick();
+            $interval(tick, internalInterval);      // schedule the interval to run the tick every internalInterval
+            tick();                                 // start the first tick
         };
 
         var service = {
-            register: function (id, tickHandler, interval, delay) {
+
+            /**
+             *
+             * Register a new task for the TickerSrv to invoke.
+             *
+             * @param id
+             *              The task ID.
+             * @param tickHandler
+             *              The task handler function. This function should return a promise.
+             * @param interval
+             *              The interval (ms) in which the task will be invoked.
+             *              Default: 1000
+             * @param delay
+             *              The delay (ms) until the first invocation.
+             *              Default: 0
+             * @param isBlocking
+             *              Should we wait for the task invocation to complete before invoking it again.
+             *              Default: true
+             *
+             */
+            register: function (id, tickHandler, interval, delay, isBlocking) {
+
+                if (interval === undefined) {
+                    interval = 1000;
+                }
+
+                if (delay === undefined) {
+                    delay = 0;
+                }
+
+                if (isBlocking === undefined) {
+                    isBlocking = true;
+                }
+
                 registrants[id] = {
-                    tick: tickHandler,        // tick handler function.
-                    interval: interval,       // configured interval.
-                    delay: delay              // delay until first tick.
+                    id: id,
+                    tick: tickHandler,
+                    interval: interval,
+                    delay: delay,
+                    isBlocking: isBlocking,
+                    isPending: false             // is the task pending, i.e. waiting to the invocation to complete
                 };
             },
 
+            /**
+             *
+             * Unregister a registered task. It will stop being invoked.
+             *
+             * @param id
+             *            The task ID
+             */
             unregister: function (id) {
                 delete registrants[id];
             },
 
-            tick: function () {
-                angular.forEach(registrants, function (registrant) {
-                    // update the delay.
-                    registrant.delay -= internalInterval;
-
-                    if (registrant.delay <= 0) {
-                        // time to tick!
-                        registrant.tick();
-
-                        //reset delay to configured interval
-                        registrant.delay = registrant.interval;
-                    }
-                });
+            /**
+             *
+             * Unregisters ALL tasks in the tasks registry. A clean slate.
+             *
+             */
+            unregisterAll: function () {
+                registrants = {};
             }
+
         };
 
         start();
